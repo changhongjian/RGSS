27f4a329387b0967c7fa3be5e39943d30fb9e0f6
taroxd <taroxd@163.com>
2015-01-01 21:21:58 +0800

    refactor

--- scripts/敌人显血.rb
+++ scripts/敌人显血.rb
@@ -13,19 +13,37 @@ class RPG::Enemy < RPG::BaseItem
   note_any :hp_pos_adjust,[0,0],/\s+(-?\d+)\s+(-?\d+)/,'[$1.to_i, $2.to_i]'
   note_i :hp_width, 80
   note_bool :hide_hp?
+
+  # 初始化宽高
+  def init_width_height
+    bitmap = Bitmap.new("Graphics/Battlers/#{@battler_name}")
+    @width = bitmap.width
+    @height = bitmap.height
+    bitmap.dispose
+  end
+
+  # 宽度
+  def width
+    return @width if @width
+    init_width_height
+    @width
+  end
+
+  # 高度
+  def height
+    return @height if @height
+    init_width_height
+    @height
+  end
 end
 
 class Sprite_EnemyHP < Sprite
   include Taroxd::RollGauge
-  #--------------------------------------------------------------------------
-  # ● 常量设定
-  #--------------------------------------------------------------------------
+
   HP_COLOR1 = Color.new(223, 127, 63)
   HP_COLOR2 = Color.new(239, 191, 63)
   BACK_COLOR = Color.new(31, 31, 63)
-  #--------------------------------------------------------------------------
-  # ● 初始化
-  #--------------------------------------------------------------------------
+
   def initialize(viewport, enemy)
     super(viewport)
     @gauge_transitions = Taroxd::Transition.new(gauge_roll_times) do 
@@ -41,23 +59,17 @@ class Sprite_EnemyHP < Sprite
       refresh
     end
   end
-  #--------------------------------------------------------------------------
-  # ● 更新
-  #--------------------------------------------------------------------------
+
   def update
     @gauge_transitions.update
     refresh if @gauge_transitions.changing
   end
-  #--------------------------------------------------------------------------
-  # ● 释放
-  #--------------------------------------------------------------------------
+
   def dispose
     bitmap.dispose if bitmap
     super
   end
-  #--------------------------------------------------------------------------
-  # ● 刷新
-  #--------------------------------------------------------------------------
+
   def refresh
     bitmap.clear if bitmap
     rate = @gauge_transitions.value
@@ -69,20 +81,15 @@ class Sprite_EnemyHP < Sprite
 end
 
 class Spriteset_Battle
-  #--------------------------------------------------------------------------
-  # ● 创建敌人血条
-  #--------------------------------------------------------------------------
+
+  # 导入精灵组
   def_after :create_enemies do
     @enemy_hp_sprites = $game_troop.members.reverse.collect do |enemy|
       Sprite_EnemyHP.new(@viewport1, enemy)
     end
   end
-  #--------------------------------------------------------------------------
-  # ● 更新敌人血条
-  #--------------------------------------------------------------------------
+
   def_after(:update_enemies) { @enemy_hp_sprites.each(&:update) }
-  #--------------------------------------------------------------------------
-  # ● 释放敌人血条
-  #--------------------------------------------------------------------------
+
   def_after(:dispose_enemies) { @enemy_hp_sprites.each(&:dispose) }
 end
