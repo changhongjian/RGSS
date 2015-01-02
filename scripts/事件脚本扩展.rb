7a208e0ed1236d9edb8986dee8e74ff28f41756b
taroxd <taroxd@163.com>
2015-01-02 11:24:43 +0800

    add method: self_switch

--- scripts/事件脚本扩展.rb
+++ scripts/事件脚本扩展.rb
@@ -9,8 +9,9 @@
 #    Game_Interpreter
 #      this_event: 获取本事件。如果事件不在当前地图上，返回 nil。
 #      add_battlelog(text): 追加战斗信息。
+#      self_switch: 返回当前事件的 SelfSwitch 对象。
 #
-#     Game_Switches/Game_Variables/Game_SelfSwitches
+#    Game_Switches/Game_Variables/Game_SelfSwitches
 #       clear / reset: 清空数据
 #
 #    Game_CharacterBase
@@ -20,14 +21,14 @@
 #         pattern 从左到右分别为 0, 1, 2。
 #         使用此功能时，建议勾选固定朝向，并且取消步行动画。
 #
-#     Game_Player
+#    Game_Player
 #       waiting 属性：设为真值时，禁止玩家移动
-#       wait_for { block }: 
+#       wait_for { block }:
 #         执行 block 期间玩家不能移动。
 #         请注意不要在 block 中 return。
 #         如果一定需要的话，记得将 waiting 属性设为伪值，使玩家重新可以移动。
 #
-#     Game_Party
+#    Game_Party
 #       +(gold), -(gold): 增加/减少金钱，并返回 self。
 #       <<(actor), <<(actor_id): 加入指定队员，并返回 self。
 #
@@ -35,6 +36,7 @@
 
 module Taroxd::EventCommandExt
 
+  # 定义了清除数据的方法
   module ClearData
 
     Game_Switches.send     :include, self
@@ -51,9 +53,39 @@ module Taroxd::EventCommandExt
 
   end
 
+  # 代表独立开关的对象
+  class SelfSwitch
+
+    def initialize(map_id, event_id)
+      @map_id = map_id
+      @event_id = event_id
+    end
+
+    def [](letter)
+      $game_self_switches[[@map_id, @event_id, letter]]
+    end
+
+    def []=(letter, value)
+      $game_self_switches[[@map_id, @event_id, letter]] = value
+    end
+
+    def a; self['A']; end
+    def b; self['B']; end
+    def c; self['C']; end
+    def d; self['D']; end
+
+    def a=(v); self['A'] = v; end
+    def b=(v); self['B'] = v; end
+    def c=(v); self['C'] = v; end
+    def d=(v); self['D'] = v; end
+  end
+
 end
 
+
 class Game_Interpreter
+  
+  include Taroxd::EventCommandExt
 
   def this_event
     $game_map.events[@event_id] if same_map?
@@ -63,6 +95,10 @@ class Game_Interpreter
     if SceneManager.scene_is?(Scene_Battle)
       SceneManager.scene.add_battlelog(text)
     end
+  end
+
+  def self_switch
+    @self_switch ||= SelfSwitch.new(@map_id, @event_id)
   end
 
 end
@@ -79,7 +115,7 @@ end
 
 
 class Game_Player < Game_Character
-  
+
   attr_accessor :waiting
 
   def_unless :movable?, :waiting
@@ -90,7 +126,7 @@ class Game_Player < Game_Character
     yield
     @waiting = false
   end
-  
+
 end
 
 class Game_Party < Game_Unit
